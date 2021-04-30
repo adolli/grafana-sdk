83554ea5303796e6fdc904cf8f273b0144424c94
wengdongdong <wengdongdong@bytedance.com>
2021-04-30 11:21:26 +0800

    fix: rest-team

--- rest-team.go
+++ rest-team.go
@@ -66,6 +66,32 @@ func (r *Client) SearchTeamsWithPaging(ctx context.Context, query *string, perpa
 	return pageTeams, err
 }
 
+func (r *Client) GetTeam(ctx context.Context, name string) (Team, error) {
+	var (
+		raw       []byte
+		pageTeams PageTeams
+		code      int
+		err       error
+	)
+	params := url.Values{}
+	if raw, code, err = r.get(ctx, "api/teams/search", params); err != nil {
+		return Team{}, err
+	}
+
+	if code != 200 {
+		return Team{}, fmt.Errorf("HTTP error %d: returns %s", code, raw)
+	}
+	dec := json.NewDecoder(bytes.NewReader(raw))
+	dec.UseNumber()
+	if err := dec.Decode(&pageTeams); err != nil {
+		return Team{}, fmt.Errorf("unmarshal users: %s\n%s", err, raw)
+	}
+	if pageTeams.TotalCount != 1 {
+		return Team{}, fmt.Errorf("teams num != 1")
+	}
+	return pageTeams.Teams[0], err
+}
+
 func (r *Client) GetAllTeam(ctx context.Context) ([]Team, error) {
 	query := ""
 	perpage := 99999
@@ -79,4 +105,37 @@ func (r *Client) GetAllTeam(ctx context.Context) ([]Team, error) {
 
 func (r *Client) GetUserTeams(ctx context.Context) {
 
+}
+
+func (r *Client) CreateTeam(ctx context.Context, team Team) (StatusMessage, error) {
+	var (
+		raw  []byte
+		resp StatusMessage
+		err  error
+	)
+	if raw, err = json.Marshal(team); err != nil {
+		return StatusMessage{}, err
+	}
+	if raw, _, err = r.post(ctx, "api/teams", nil, raw); err != nil {
+		return StatusMessage{}, err
+	}
+	if err = json.Unmarshal(raw, &resp); err != nil {
+		return StatusMessage{}, err
+	}
+	return resp, nil
+}
+
+func (r *Client) DeleteTeam(ctx context.Context, id uint) (StatusMessage, error) {
+	var (
+		raw  []byte
+		resp StatusMessage
+		err  error
+	)
+	if raw, _, err = r.delete(ctx, fmt.Sprintf("api/teams/%d", id)); err != nil {
+		return StatusMessage{}, err
+	}
+	if err = json.Unmarshal(raw, &resp); err != nil {
+		return StatusMessage{}, err
+	}
+	return resp, nil
 }
